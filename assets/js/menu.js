672f882aece30436318813e509d57bad6c52ccc7
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-03-01 16:38:49 -0400

    updated

---
+++ assets/js/menu.js
@@ -0,0 +1,23 @@
+let btnMobile = document.querySelector('.btn-mobile');
+
+
+function toggleMenu(event){
+    if(event.type === 'touchstart'){
+        event.preventDefault();
+    }
+
+    let nav = document.querySelector('.menu');
+    nav.classList.toggle('active');
+    
+    if(nav.classList.contains('active')){
+        document.documentElement.style.overflow = 'hidden';
+        document.body.scroll = "no"; 
+    }else{
+        document.documentElement.style.overflow = 'auto';
+        document.body.scroll = "yes"; 
+    }
+}
+
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
